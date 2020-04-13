fe40ed8f71a56373787938b34d199bcb55eff6df
d3jw <dejw.sedlar@gmail.com>
2020-04-13 20:10:28 +0200

    whole card clickable

--- src/components/Herbs/HerbCard.js
+++ src/components/Herbs/HerbCard.js
@@ -5,7 +5,7 @@ import { Button } from "../Atoms/Buttons";
 
 const HerbCard = ({ plant, handleDetailClicked }) => {
   return (
-    <Card style={{ width: "30rem", marginBottom: "20px", marginTop: "20px" }}>
+    <Card style={{ width: "30rem", marginBottom: "20px", marginTop: "20px" }} onClick={handleDetailClicked}>
       <Card.Img variant="top" src={plant.img} style={{ height: "25rem" }} />
       <Card.Body>
         <Card.Title style ={{color: "#526e35"}}>{plant.familyName}</Card.Title>
