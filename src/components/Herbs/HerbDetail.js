681583bdc1428836ac5046740b48892a5eb3b6b6
d3jw <dejw.sedlar@gmail.com>
2020-04-13 17:54:26 +0200

    detail page

--- src/components/Herbs/HerbDetail.js
+++ src/components/Herbs/HerbDetail.js
@@ -3,19 +3,51 @@ import TemplateView from "../TemplateView/TemplateView";
 
 import { useParams } from "react-router-dom";
 
-import plants from '../../data/plants.json'
+import styled from "@emotion/styled";
+
+import { Row, Col } from "react-bootstrap";
+import plants from "../../data/plants.json";
+
+const AboutCard = styled.div({
+  marginLeft: "10vw",
+  marginRight: "10vw",
+  paddingTop: "5vh",
+  paddingBottom: "5vh",
+});
 
 const HerbDetail = () => {
-
   const { id } = useParams();
-  const plant = plants.find(plant => plant.id === parseInt(id));
+  const plant = plants.find((plant) => plant.id === parseInt(id));
 
   console.log(plant);
   return (
     <TemplateView header={plant.familyName}>
-        <div style={{height: "80vh"}}>
-
-        </div>
+      <div style={{ height: "80vh" }}>
+        <AboutCard>
+          <Row>
+            <Col>
+              <img
+                src={plant.img}
+                alt={plant.id}
+                style={{
+                  boxSizing: "border-box",
+                  objectFit: "cover",
+                  display: "block",
+                  margin: "auto",
+                  width : "100%",
+                  height : "100%"
+                }}
+              ></img>
+            </Col>
+            <Col>
+              <h1>Family name: {plant.commonName}</h1>
+              <h2>Scientific name: {plant.scintificName}</h2>
+              <br></br>
+              <p>About: {plant.about}</p>
+            </Col>
+          </Row>
+        </AboutCard>
+      </div>
     </TemplateView>
   );
 };
