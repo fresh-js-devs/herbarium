545035f8251b48118eadd5bff62af84d3607e62c
d3jw <dejw.sedlar@gmail.com>
2020-04-13 17:23:12 +0200

    text colors

--- src/components/Herbs/HerbCard.js
+++ src/components/Herbs/HerbCard.js
@@ -8,7 +8,7 @@ const HerbCard = ({ plant, handleDetailClicked }) => {
     <Card style={{ width: "30rem", marginBottom: "20px", marginTop: "20px" }}>
       <Card.Img variant="top" src={plant.img} style={{ height: "25rem" }} />
       <Card.Body>
-        <Card.Title>{plant.familyName}</Card.Title>
+        <Card.Title style ={{color: "#526e35"}}>{plant.familyName}</Card.Title>
         <Card.Text style={{ minHeight: "15rem" }}>{plant.about}</Card.Text>
         <Button variant="info" onClick={handleDetailClicked}>
           Detail
