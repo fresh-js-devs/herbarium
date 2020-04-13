de4b9bc7b90fe552814d51121b11ea072a99c349
d3jw <dejw.sedlar@gmail.com>
2020-04-13 14:25:14 +0200

    bootstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 
 import Routes from "./components/routes/Routes";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.css";
+
 const App = () => <Routes />;
 
 export default App;
