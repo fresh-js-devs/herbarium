ce02f1a74a8d95fee75375eb6bb6d836f31c81a8
d3jw <dejw.sedlar@gmail.com>
2020-04-13 16:46:19 +0200

    atoms, button

---
+++ src/components/Atoms/Buttons.js
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const Button = styled.button({
+  minWidth: "70px",
+  minHeight: "50px",
+  backgroundColor: "#9ed166",
+  color: "#526e35",
+  borderRadius: "25px",
+  border: "2px solid #526e35",
+  alignItems: "center",
+  alignContent: "center",
+  textAlign: "center",
+  verticalAlign: "center",
+  fontWeight: "bold"
+}) ;
--- src/components/Herbs/HerbCard.js
+++ src/components/Herbs/HerbCard.js
@@ -1,14 +1,15 @@
 import React from "react";
 
-import { Card, Button } from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import { Button } from "../Atoms/Buttons";
 
 const HerbCard = ({ plant }) => {
   return (
-    <Card style={{ width: "30rem", marginBottom: "20px" , marginTop: "20px"}}>
-      <Card.Img variant="top" src={plant.img} />
+    <Card style={{ width: "30rem", marginBottom: "20px", marginTop: "20px" }}>
+      <Card.Img variant="top" src={plant.img} style={{ height: "25rem" }} />
       <Card.Body>
         <Card.Title>{plant.familyName}</Card.Title>
-        <Card.Text>{plant.about}</Card.Text>
+        <Card.Text style={{ minHeight: "15rem" }}>{plant.about}</Card.Text>
         <Button variant="info">Detail</Button>
       </Card.Body>
     </Card>
